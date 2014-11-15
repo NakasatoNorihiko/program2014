64ee4d2ad3a81d7e2d9fb51b1a1f0df40ea9e3b5
Norihiko Nakasato <n.norihiko1229@gmail.com>
2014-11-15 15:54:57 +0900

    kadai03 reform1

--- lec06/kadai03.c
+++ lec06/kadai03.c
@@ -2,45 +2,98 @@
 #include <stdlib.h>
 #include <string.h>
 
+int s_strlen(char *);            /* 文字列の長さを測る関数 */
+void s_strcpy(char *, char *);   /* 文字列をコピーする関数 */
+char**  s_strcut(char *ori, char *cut, int *cutn); /* 文字列oriをcutに含まれる文字で切り分けて、dataに入れる関数 datalenにdataの要素数を返す */
+
 int main(int argc, char *argv[]) 
 {
-    char *pol;
     char **mon;
-    char tmp[20];
-    /* int state = 1;              /\* 0なら単項式の外 *\/ */
-    
-    int i,j,k,n;                  /* nは単項式の数 */
-    int len = 0;                      /* 全体の長さ */
-    i = j = n = 0;
-    strcpy(tmp, "");
 
-    while(argv[1][len] != '\0') {
-        len++;
-    }
-    mon = (char **)malloc(sizeof(char*) * (len+1));
+    int monn;                 /* monnは単項式の数 */
+    int i,j; 
+    int len;
+
+    len = s_strlen(argv[1]);
     
-    i = 0;
-    for (i = 0; i <= len; i++) {
-        if (argv[1][i] != '+' && argv[1][i] != '-' && argv[1][i] != '\0') {
-            tmp[j] = argv[1][i];
-            j++;
-        } else {
-            tmp[j] = '\0';
-            mon[n] = (char *)malloc(sizeof(char) * (j+1));
-            for (k = 0; k < j+2; k++) {
-                mon[n][k] = tmp[k];
-            }
-            j = 0;
-            if (argv[1][i] == '-') {
-                tmp[j] = argv[1][i];
-                j++;
-            }
-            n++;
+    mon = s_strcut(argv[1], "+-", &monn);
+    
+    for (i = 0; i < monn; i++) {
+      if (mon[i][0] == '+') {
+        for (j = 0; j < s_strlen(mon[i]); j++) {
+          mon[i][j] = mon[i][j+1];
         }
+      }
     }
     printf("Polynomial = %s\n", argv[1]);
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < monn; i++) {
         printf("Monomial %d = %s\n", i+1, mon[i]);
     }
     return 0;
+}
+
+int s_strlen(char *str)
+{
+  int len = 0;
+  while(str[len] != '\0') {
+    len++;
+  }
+  return len;
+}
+
+void s_strcpy(char *str1, char *str2)
+{
+  int len;
+  int i;
+  len = s_strlen(str2);
+  for (i = 0; i <= len; i++) {
+    str1[i] = str2[i];
+  }
+  return;
+}
+
+/* 文字列oriをcutに含まれる文字で切り分けて、dataに入れる関数 datalenにdataの要素数を返す */
+char** s_strcut(char *ori, char *cut, int *datalen) {
+  int i, j, k, n;
+  char **data;
+  int orilen = s_strlen(ori);
+  int cutn = s_strlen(cut);
+  char tmp[1000];
+  int state;
+
+  *datalen = 1;                  /* 何個に分割されるかを示す */
+  n = 0;
+  for(i = 0; i < orilen; i++) {
+    for (j = 0; j < cutn; j++) {
+      if (ori[i] == cut[j]) {
+        (*datalen)++;
+      }
+    }
+  }
+  data = (char **)malloc(sizeof(char*)*(*datalen));
+
+  k = 0;
+  for (i = 0; i <= orilen; i++) {
+    state = 0;
+    for (j = 0; j < cutn; j++) {
+      if (ori[i] == '\0') {
+        state = 1;
+     } else if (ori[i] == cut[j]) {
+        state = 1;
+      }
+    }
+    if (state == 1){
+      tmp[k] = '\0';
+      data[n] = (char*)malloc(sizeof(char) * (k+1));
+      s_strcpy(data[n], tmp);
+      tmp[0] = ori[i];
+      k = 1;
+      n++;
+    } else {
+      tmp[k] = ori[i];
+      k++;
+    }
+  }
+
+  return data;
 }
